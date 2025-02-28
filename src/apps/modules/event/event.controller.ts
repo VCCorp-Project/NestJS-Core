d53598eeae09ca090b87b29c52e3182fe65ce2d9
aten040791 <aten040791@gmail.com>
2025-03-01 01:27:57 +0700

    Optimize code

--- src/apps/modules/event/event.controller.ts
+++ src/apps/modules/event/event.controller.ts
@@ -4,13 +4,11 @@ import {
   Get,
   HttpStatus,
   Post,
-  Res,
   UploadedFiles,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { EventService } from './event.service';
-import { Response } from 'express';
 import { FileService } from 'src/core/modules/file/file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateEventDto } from './dto/event.dto';
@@ -37,18 +35,12 @@ export class EventController {
   async store(
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Body() createEventDto: CreateEventDto,
-    @Res() res: Response,
   ) {
     createEventDto.coverImage = files[0].originalname;
     const event = await this.eventService.store(createEventDto);
-    return res.status(HttpStatus.OK).json({
-      success: true,
-      data: {
-        event,
-        location: files[0].destination,
-      },
-      status: HttpStatus.CREATED,
-      message: 'ok',
-    });
+    return {
+      event,
+      location: files[0].destination,
+    };
   }
 }
